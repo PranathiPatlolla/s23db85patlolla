829f39ab49d09d54bc6d8050f80615acd9b14e08
PranathiPatlolla <ppranathireddy16@gmail.com>
2023-04-13 22:13:23 -0500

    added code

--- controllers/emoji.js
+++ controllers/emoji.js
@@ -19,9 +19,29 @@ exports.emoji_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: emoji detail: ' + req.params.id);
 };
 // Handle emoji create on POST.
-exports.emoji_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: emoji create POST');
+
+//res.send('NOT IMPLEMENTED: emoji create POST');
+// Handle Costume create on POST.
+exports.emoji_create_post = async function(req, res) {
+console.log(req.body)
+let document = new emoji();
+// We are looking for a body, since POST does not have query parameters.
+// Even though bodies can be in many different formats, we will be picky
+// and require that it be a json object
+// {"costume_type":"goat", "cost":12, "size":"large"}
+document.emoji_face = req.body.emoji_face;
+document.emoji_hand = req.body.emoji_hand;
+document.emoji_things = req.body.emoji_things;
+try{
+let result = await document.save();
+res.send(result);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
 };
+
 // Handle emoji delete form on DELETE.
 exports.emoji_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: emoji delete DELETE ' + req.params.id);
@@ -30,3 +50,16 @@ res.send('NOT IMPLEMENTED: emoji delete DELETE ' + req.params.id);
 exports.emoji_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: emoji update PUT' + req.params.id);
 };
+
+// VIEWS
+// Handle a show all view
+exports.emoji_view_all_Page = async function(req, res) {
+    try{
+    theemoji = await emoji.find();
+    res.render('emoji', { title: 'emoji Search Results', results: theemoji });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
--- routes/emoji.js
+++ routes/emoji.js
@@ -1,8 +1,6 @@
 var express = require('express');
+const emoji_controlers= require('../controllers/emoji');
 var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('emoji', { title: 'Emoji reactions of human' });
-});
-
+/* GET costumes */
+router.get('/', emoji_controlers.emoji_view_all_Page );
 module.exports = router;
