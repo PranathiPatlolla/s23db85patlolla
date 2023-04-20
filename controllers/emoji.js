cf3239fa8b0665865d8053930c4051e57cd6e677
PranathiPatlolla <ppranathireddy16@gmail.com>
2023-04-19 20:48:11 -0500

    update code

--- controllers/emoji.js
+++ controllers/emoji.js
@@ -55,9 +55,9 @@ res.send(`{"error": ${err}}`);
 };
 
 // Handle emoji delete form on DELETE.
-exports.emoji_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: emoji delete DELETE ' + req.params.id);
-};
+//exports.emoji_delete = function(req, res) {
+////res.send('NOT IMPLEMENTED: emoji delete DELETE ' + req.params.id);
+//};
 // Handle emoji update form on PUT.
 //exports.emoji_update_put = function(req, res) {
 //res.send('NOT IMPLEMENTED: emoji update PUT' + req.params.id);
@@ -95,3 +95,71 @@ exports.emoji_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+    // Handle Costume delete on DELETE.
+exports.emoji_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await emoji.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
+
+   // Handle a show one view with id specified by query
+exports.emoji_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await emoji.findById( req.query.id)
+    res.render('emojidetail',
+   { title: 'emoji Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.emoji_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('emojicreate', { title: 'emoji Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for updating a costume.
+// query provides the id
+exports.emoji_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await emoji.findById(req.query.id)
+    res.render('emojiupdate', { title: 'emoji Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle a delete one view with id from query
+exports.emoji_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await emoji.findById(req.query.id)
+    res.render('emojidelete', { title: 'emoji Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/emoji.js
+++ routes/emoji.js
@@ -4,3 +4,11 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', emoji_controlers.emoji_view_all_Page );
 module.exports = router;
+/* GET detail costume page */
+router.get('/detail', emoji_controlers.emoji_view_one_Page);
+/* GET create costume page */
+router.get('/create', emoji_controlers.emoji_create_Page);
+/* GET create update page */
+router.get('/update', emoji_controlers.emoji_update_Page);
+/* GET delete costume page */
+router.get('/delete', emoji_controlers.emoji_delete_Page);
